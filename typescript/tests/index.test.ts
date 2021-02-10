74bdb2d4faee475606fc8c4fe76ad481d03c84e8
DCsunset <dcsunset@protonmail.com>
2021-02-10 17:15:04 +0800

    test: add tests

---
+++ typescript/tests/index.test.ts
@@ -0,0 +1,35 @@
+import { initTaskJson, isTaskJson } from "task.json";
+import { Client } from "../lib";
+
+describe("Client API", () => {
+	const client = new Client("http://localhost:3000");
+
+	test("invalid login", async () => {
+		await expect(client.login("test")).rejects.toThrow();
+	});
+
+	test("valid login", async () => {
+		await client.login("admin");
+	});
+
+	test("download", async () => {
+		const tj = await client.download();
+		expect(isTaskJson(tj)).toBe(true);
+	});
+
+	test("sync", async () => {
+		const tj1 = initTaskJson();
+		const tj2 = await client.sync(tj1);
+		expect(isTaskJson(tj2)).toBe(true);
+	});
+
+
+	test("upload", async () => {
+		const tj1 = initTaskJson();
+		await client.upload(tj1);
+	});
+
+	test("logout", async () => {
+		await client.logout();
+	});
+});
