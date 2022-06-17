e5c4bcdbbb0e33631817dfbe612f07c191049ebc
DCsunset <DCsunset@users.noreply.github.com>
2022-06-17 16:06:02 -0400

    test(typescript): add new tests for https

--- typescript/tests/index.test.ts
+++ typescript/tests/index.test.ts
@@ -1,8 +1,15 @@
 import { initTaskJson, isTaskJson } from "task.json";
-import { Client } from "../lib";
+import { Client, setupClient } from "../lib";
+import { X509Certificate } from "node:crypto";
 
-describe("Client API", () => {
-	const client = new Client("http://localhost:3000");
+describe("Connect to HTTP Server", () => {
+	let client: Client;
+	
+	test("setup client", async () => {
+		client = await setupClient({
+			server: "http://localhost:3000"
+		});
+	});
 
 	test("invalid login", async () => {
 		await expect(client.login("test")).rejects.toThrow();
@@ -32,4 +39,43 @@ describe("Client API", () => {
 	test("logout", async () => {
 		await client.logout();
 	});
+});
+
+// Self-signed cert
+describe("Connect to HTTPS Server", () => {
+	let client1: Client;
+	let client2: Client;
+	
+	test("setup client 1", async () => {
+		client1 = await setupClient({
+			server: "https://localhost:8000"
+		});
+	});
+
+	test("reject self-signed cert", async () => {
+		await expect(client1.login("admin")).rejects.toThrow();
+	});
+	
+	let cert: X509Certificate | undefined;
+	test("get certificate", async () => {
+		cert = await client1.getCertificate();
+		expect(cert).toBeTruthy();
+	})
+
+	test("setup client 2", async () => {
+		client2 = await setupClient({
+			server: "https://localhost:8000",
+			ca: cert?.toString()
+		});
+	});
+
+	test("valid login", async () => {
+		await client2.login("admin");
+	});
+
+	test("get certificate", async () => {
+		const cert = await client2.getCertificate();
+		console.log(cert?.fingerprint);
+		expect(cert).toBeTruthy();
+	})
 });
