5d889e3ef75d34453ab422962e4ba434caa9cba8
DCsunset <DCsunset@users.noreply.github.com>
2022-06-17 16:05:01 -0400

    fix(typescript): fix error handling

--- typescript/lib/errors.ts
+++ typescript/lib/errors.ts
@@ -10,11 +10,11 @@ export function handleAxiosError(error: AxiosError): never {
 	if (error.response) {
 		// The request was made and the server responded with a status code
 		// that falls out of the range of 2xx	
-		throw new HttpError(error.response.status, error.response.data);
+		throw new HttpError(error.response.status, error.response.data as string);
 	}
 	else if (error.request) {
 		// The request was made but no response was received
-		throw new HttpError(503, "No response from server");
+		throw new HttpError(503, error.message);
 	}
 	else {
 		// Something happened in setting up the request that triggered an Error
