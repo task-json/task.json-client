bd1cc9b2166ab5fc6ca568d3c8ebe6d7583a4deb
DCsunset <DCsunset@users.noreply.github.com>
2022-06-17 16:06:21 -0400

    test(typescript): fix esm import

--- typescript/jest.config.js
+++ typescript/jest.config.js
@@ -1,4 +1,11 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  // See https://kulshekhar.github.io/ts-jest/docs/guides/esm-support/
+  extensionsToTreatAsEsm: ['.ts'],
+  globals: {
+    'ts-jest': {
+      useESM: true
+    }
+  }
 };
